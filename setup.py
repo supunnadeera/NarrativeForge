cc7d98929439ab15c7f809d65c757ba3df879c97
supunnadeera <129364689+supunnadeera@users.noreply.github.com>
2024-02-02 11:52:33 +0530

    Revert "added cli commands"
    This reverts commit 712d0998dae611c978afb8dbbc81e714449eb871.

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="NarrativeForge",
-    version="0.1.1",
+    version="0.1.0",
     description='A package for adding text and ai generated voice to videos.',
     author='supun nadeera',
-    url='https://github.com/supunnadeera/NarrativeForge',
-    include_package_data=True,
+     url='https://github.com/supunnadeera/NarrativeForge',
     packages=find_packages(),
     install_requires=[
         "numpy",
