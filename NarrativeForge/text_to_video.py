712d0998dae611c978afb8dbbc81e714449eb871
supunnadeera <129364689+supunnadeera@users.noreply.github.com>
2024-02-02 11:45:42 +0530

    added cli commands

--- NarrativeForge/text_to_video.py
+++ NarrativeForge/text_to_video.py
@@ -1,4 +1,5 @@
 import os
+import click
 from moviepy.editor import VideoFileClip, CompositeVideoClip, TextClip, AudioFileClip
 from .text_to_speech import text_to_speech_instance
 from NarrativeForge.speech_to_text import speech_to_text_instance
@@ -8,7 +9,7 @@ class TextToVideo:
     def __init__(self):
         self.audio_file = "test.mp3"
         self.fps = 30
-    
+
     def _create_text_clips(self, segments, caption_size, text_clip_params):
         txt_clips = []
 
@@ -48,25 +49,24 @@ class TextToVideo:
     def show_available_colors(self):
         print(TextClip.list("color"))
 
+    @click.command()
+    @click.option("--input-file", "-i", help="Input video file", required=True)
+    @click.option("--texts", "-t", help="Texts to be added to the video", required=True)
+    @click.option("--voice", default="shimmer", help="The voice to use for speech generation")
+    @click.option("--output-filename", default="output.mp4", help="Path for the output video file")
+    @click.option("--caption-padding", default=100, help="Padding for the captions")
     def generate_video(
-        self,
-        input_filename,
-        texts,
-        voice="shimmer",
-        output_filename="output.mp4",
-        caption_padding=100,
-        text_clip_params=None,
+        self, input_file, texts, voice, output_filename, caption_padding
     ):
         """
         Generate a video with text overlays.
 
         Parameters:
-        - input_filename (str): Path to the input video file.
+        - input_file (str): Path to the input video file.
         - texts (List[str]): List of texts to overlay on the video.
-        - output_filename (str): Path for the output video file (default is 'output.mp4').
         - voice (str): The voice to use for speech generation (available: shimmer(default), nova, onyx, fable, echo, alloy ).
+        - output_filename (str): Path for the output video file (default is 'output.mp4').
         - caption_padding (int): Padding for the captions (default is 100).
-        - text_clip_params (Optional[Dict[str, any]]): Additional parameters for MoviePy TextClip.
         Returns:
         - None
         """
@@ -74,14 +74,14 @@ class TextToVideo:
         segmented_texts = speech_to_text_instance.generate_text(self.audio_file)
         clip_duration = segmented_texts[-1]["end"]
 
-        clip = VideoFileClip(os.path.join(os.getcwd(), input_filename))
+        clip = VideoFileClip(os.path.join(os.getcwd(), input_file))
         clip = clip.without_audio()
         clip = clip.subclip(0, clip_duration)
         audio_clip = AudioFileClip(os.path.join(os.getcwd(), self.audio_file))
 
         caption_size = clip.size[1] - caption_padding
         txt_clips = self._create_text_clips(
-            segmented_texts, caption_size, text_clip_params
+            segmented_texts, caption_size, text_clip_params=None
         )
         video = CompositeVideoClip([clip.set_audio(audio_clip)] + txt_clips)
         video.write_videofile(
@@ -90,3 +90,18 @@ class TextToVideo:
 
 
 text_to_video_instance = TextToVideo()
+@click.command()
+@click.option("--input-file", "-i", help="Input video file", required=True)
+@click.option("--texts", "-t", help="Texts to be added to the video", required=True)
+@click.option("--voice", default="shimmer", help="The voice to use for speech generation")
+@click.option("--output-filename", default="output.mp4", help="Path for the output video file")
+@click.option("--caption-padding", default=100, help="Padding for the captions")
+def main(input_file, texts, voice, output_filename, caption_padding):
+    text_to_video_instance = TextToVideo()
+    text_to_video_instance.generate_video(
+        input_file, texts, voice, output_filename, caption_padding
+    )
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="NarrativeForge",
-    version="0.1.0",
+    version="0.1.1",
     description='A package for adding text and ai generated voice to videos.',
     author='supun nadeera',
-     url='https://github.com/supunnadeera/NarrativeForge',
+    url='https://github.com/supunnadeera/NarrativeForge',
+    include_package_data=True,
     packages=find_packages(),
     install_requires=[
         "numpy",
